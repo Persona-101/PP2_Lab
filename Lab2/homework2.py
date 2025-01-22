1b45773c6ba4894bf0d2e82745e8c75f45eb5f59
Sabina Khalidullina <s.halidullina@gmail.com>
2025-01-22 15:53:24 +0500

    Update homework2.py

--- Lab2/homework2.py
+++ Lab2/homework2.py
@@ -1,1 +1,20 @@
+#boolean
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("Abc"))
+print(bool(""))
+print(bool(15))
+print(bool(0))
+print(bool(["a", "b", "c"]))
+print(bool([]))
+print(bool(None))
 
