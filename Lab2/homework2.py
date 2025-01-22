fec8717b61897d7215d14388bbe21951d6a916f6
Sabina Khalidullina <s.halidullina@gmail.com>
2025-01-22 15:57:29 +0500

    Update homework2.py

--- Lab2/homework2.py
+++ Lab2/homework2.py
@@ -3,12 +3,14 @@ print(10 > 9)
 print(10 == 9)
 print(10 < 9)
 
+
 a = 200
 b = 33
 if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
+
 
 print(bool("Abc"))
 print(bool(""))
@@ -17,4 +19,23 @@ print(bool(0))
 print(bool(["a", "b", "c"]))
 print(bool([]))
 print(bool(None))
+
+
+class myclass():
+  def __len__(self):
+    return 0
+myobj = myclass()
+print(bool(myobj))
+
+
+def myFunction() :
+  return True
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+
+x = 200
+print(isinstance(x, int))
 
